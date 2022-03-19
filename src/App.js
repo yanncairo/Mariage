0ad71b551562400c1460545002069314ccb0a8d2
Yann Louis-Albert AQUILON <yann.louis-albert.aquilon.20171010@esigetel.net>
2022-03-19 11:26:18 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <Navbar/>
+        </Router>     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
